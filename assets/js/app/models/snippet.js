2ff829273b7b891be83f4f32299d1724f26b8f79
Nicolas Clerc <kernicPanel@nclerc.fr>
2013-08-10 15:12:16 +0200

    add tags relation to snippet model

--- assets/js/app/models/snippet.js
+++ assets/js/app/models/snippet.js
@@ -2,6 +2,10 @@ Sharecode.Snippet = DS.Model.extend({
   name: DS.attr('string'),
   language: DS.attr('string'),
   body: DS.attr('string'),
+  tags: DS.hasMany('Sharecode.Tag'),
+  tagsNames: function(){
+    return tags.getEach('name');
+  }.property('tags'),
   isCompleted: DS.attr('boolean'),
 
   /*
